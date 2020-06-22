9b2991596f81cca9fb3445a81f00c3e2c14bc68e
SUJAY S <sujaysreedharg@gmail.com>
2020-06-22 17:53:01 +0530

    first commit

---
+++ Find the value of pi.py
@@ -0,0 +1,14 @@
+import numpy as np
+import random
+def estimate_pi(n):
+    num_point_circle=0
+    num_point_total=0
+    for _ in range(n):
+        x=random.uniform(0,1)
+        y=random.uniform(0,1)
+        distance=np.sqrt((x**2)+(y**2))
+        if distance<=1:
+            num_point_circle+=1
+            num_point_total+=1
+    return 4*num_point_circle/num_point_total
+    
