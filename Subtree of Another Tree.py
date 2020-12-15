98acd3f8871656a5441fa98271e74ea50d2cdb51
SUJAY S <44267241+sujaysreedharg@users.noreply.github.com>
2020-12-15 11:46:06 +0530

    Create  Subtree of Another Tree.py

---
+++ Subtree of Another Tree.py
@@ -0,0 +1,48 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        def preOrder(node):
+            if node:
+                return '$' + str(node.val) + preOrder(node.left) + preOrder(node.right)
+            else:
+                return '#'
+        a = preOrder(s)
+        b = preOrder(t)
+        return b in a
+
+
+
+// Example 1:
+Given tree s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+Given tree t:
+   4 
+  / \
+ 1   2
+Return true, because t has the same structure and node values with a subtree of s.
+ 
+ Example 2:
+Given tree s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+    /
+   0
+Given tree t:
+   4
+  / \
+ 1   2
+Return false. It is a substring, not a sub tree.
