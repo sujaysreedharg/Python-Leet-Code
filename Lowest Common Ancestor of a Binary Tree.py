ad685e502729c16ef807555dc1aa7aef30a9936c
SUJAY S <44267241+sujaysreedharg@users.noreply.github.com>
2020-12-18 10:02:09 +0530

    Create Lowest Common Ancestor of a Binary Tree.py

---
+++ Lowest Common Ancestor of a Binary Tree.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if (root== None):
+            return None
+        if(root==p or root==q):
+            return root
+        left = self.lowestCommonAncestor(root.left,p,q)
+        right = self.lowestCommonAncestor(root.right,p,q)
+        if ( left!=None and right!=None):
+            return root
+        if (left==None and right == None):
+            return None
+        return ({True: left, False:right} [left!= None])
+        
+        
+        
+        
+        Example 1:https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+Example 2:
+
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+Output: 5
+Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+Example 3:
+
+Input: root = [1,2], p = 1, q = 2
+Output: 1
