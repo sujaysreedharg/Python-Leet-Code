d550177421c71cb079c13a3269402345c1395a5e
SUJAY S <44267241+sujaysreedharg@users.noreply.github.com>
2020-12-22 19:53:08 +0530

    Update Longest Palindromic Substring(Dynamic Prog).py

--- Longest Palindromic Substring(Dynamic Prog).py
+++ Longest Palindromic Substring(Dynamic Prog).py
@@ -1,30 +1,25 @@
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        res = ""
-        resLen = 0
-        
-        for i in range(len(s)):
-            # odd length
-            l, r = i, i
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > resLen:
-                    res = s[l:r + 1]
-                    resLen = r - l + 1
-                l -= 1
-                r += 1
-        
-            # even length
-            l, r = i, i + 1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > resLen:
-                    res = s[l:r + 1]
-                    resLen = r - l + 1
-                l -= 1
-                r += 1
-                
-        return res
+def longpalindrmesubstring(string):
+  currentlongest=[0,1]
+  for i in range(1,len(string)):
+    odd = getlongestpalindrome(string,i-1,i+1)
+    even = getlongestpalindrome(string,i-1,i)
+    longest= max(odd,even,key= lambda x : x[1] -x[0])
+    currentlongest= max(currentlongest,longest,key= lambda x : x[1]-x[0])
+  return string[currentlongest[0] : currentlongest[1]]
+def getlongestpalindrome(string,left,right):
+  while left>=0 and right<len(string):
+    if string[left]!=string[right]:
+      break
+    else:
+      left-=1
+      right+=1
+  return (left+1,right)
+print(longpalindrmesubstring("daccac"))
 
 
+
+O(n^2) -> Time
+O(1) -> Space
 
 
 
