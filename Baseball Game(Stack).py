eff47e0608844f60dd560686c88177bc000b5dfc
SUJAY S <44267241+sujaysreedharg@users.noreply.github.com>
2021-01-04 15:47:23 +0530

    Create Baseball Game(Stack).py

---
+++ Baseball Game(Stack).py
@@ -0,0 +1,62 @@
+class Solution:
+    def calPoints(self, ops: List[str]) -> int:
+        stack=[]
+        for ops in ops:
+            if ops == '+':
+                stack.append(stack[-1]+stack[-2])
+            elif ops == 'D':
+                stack.append(2*stack[-1])
+            elif ops == 'C':
+                stack.pop()
+            else:
+                stack.append(int(ops))
+        return sum(stack)
+        
+        
+Time Complexity: O(N), where NN is the length of ops. We parse through every element in the given array once, and do O(1) work for each element.
+
+Space Complexity: O(N), the space used to store our stack.
+
+
+
+You are keeping score for a baseball game with strange rules. The game consists of several rounds, where the scores of past rounds may affect future rounds' scores.
+
+At the beginning of the game, you start with an empty record. You are given a list of strings ops, where ops[i] is the ith operation you must apply to the record and is one of the following:
+
+An integer x - Record a new score of x.
+"+" - Record a new score that is the sum of the previous two scores. It is guaranteed there will always be two previous scores.
+"D" - Record a new score that is double the previous score. It is guaranteed there will always be a previous score.
+"C" - Invalidate the previous score, removing it from the record. It is guaranteed there will always be a previous score.
+Return the sum of all the scores on the record.
+
+ 
+
+Example 1:
+
+Input: ops = ["5","2","C","D","+"]
+Output: 30
+Explanation:
+"5" - Add 5 to the record, record is now [5].
+"2" - Add 2 to the record, record is now [5, 2].
+"C" - Invalidate and remove the previous score, record is now [5].
+"D" - Add 2 * 5 = 10 to the record, record is now [5, 10].
+"+" - Add 5 + 10 = 15 to the record, record is now [5, 10, 15].
+The total sum is 5 + 10 + 15 = 30.
+Example 2:
+
+Input: ops = ["5","-2","4","C","D","9","+","+"]
+Output: 27
+Explanation:
+"5" - Add 5 to the record, record is now [5].
+"-2" - Add -2 to the record, record is now [5, -2].
+"4" - Add 4 to the record, record is now [5, -2, 4].
+"C" - Invalidate and remove the previous score, record is now [5, -2].
+"D" - Add 2 * -2 = -4 to the record, record is now [5, -2, -4].
+"9" - Add 9 to the record, record is now [5, -2, -4, 9].
+"+" - Add -4 + 9 = 5 to the record, record is now [5, -2, -4, 9, 5].
+"+" - Add 9 + 5 = 14 to the record, record is now [5, -2, -4, 9, 5, 14].
+The total sum is 5 + -2 + -4 + 9 + 5 + 14 = 27.
+Example 3:
+
+Input: ops = ["1"]
+Output: 1
