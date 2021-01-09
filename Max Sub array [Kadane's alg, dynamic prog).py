642802cdc857b5af0dd063773093f3a72e11d4cf
SUJAY S <44267241+sujaysreedharg@users.noreply.github.com>
2021-01-09 11:37:29 +0530

    Update and rename Max Sub array [kadane's alg, dynamic prog).py to Max Sub array [Kadane's alg, dynamic prog).py

--- Max Sub array [Kadane's alg, dynamic prog).py
+++ Max Sub array [Kadane's alg, dynamic prog).py
@@ -10,8 +10,26 @@ class Solution:
             return maxSum
         return nums[0]
             
-            
-            
+  Or: Sliding window approach:
+ class Solution:
+    def buddyStrings(self, arr, k) -> bool:
+        max_sum=0
+        old_sum=0
+        n=len(arr)
+        for i in range(k):
+            old_sum += arr[i]
+        print(old_sum)
+    
+        for currval in range(k,n):
+
+            old_sum = old_sum- arr[currval-k] + arr[currval] 
+            max_sum = max(max_sum, old_sum)
+
+        return max_sum
+
+Sol= Solution()
+print(Sol.buddyStrings([100, 200, 300, 400],2))
+    Time complexity reduced from O(n²) to O(n) in sliding window approach.         
      Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
 Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
