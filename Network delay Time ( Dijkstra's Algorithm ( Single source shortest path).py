692b65af7cd34b88a10b16c8833b4060d90626d9
SUJAY S <44267241+sujaysreedharg@users.noreply.github.com>
2021-01-22 19:37:45 +0530

    Create Network delay Time ( Dijkstra's Algorithm ( Single source shortest path).py

---
+++ Network delay Time ( Dijkstra's Algorithm ( Single source shortest path).py
@@ -0,0 +1,28 @@
+O(n^2) Time Dijkstra using hashmap
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
+        graph=collections.defaultdict(list)
+        for edges in times:
+            if edges[0] not in graph:
+                graph[edges[0]]=[(edges[1],edges[2])]
+            else:
+                graph[edges[0]].append((edges[1],edges[2]))
+
+        dist={node: float("inf") for node in range(1,N+1)}
+        dist[K]=0
+        seen = [False]*(N+1)
+        while True:
+            cand_node = -1
+            cand_dist = float("inf")
+            for i in range(1,N+1):
+                if not seen[i] and dist[i]<cand_dist:
+                    cand_node = i
+                    cand_dist = dist[i]
+            if cand_node<0:
+                break
+            seen[cand_node]=True
+            for neigh,dis in graph[cand_node]:
+                dist[neigh]=min(dist[neigh],dist[cand_node]+dis)
+        ans=max(dist.values())
+        return ans if ans<float("inf") else -1
