b99a6adebe6b69773c0a09311130ee961d193719
SUJAY S <44267241+sujaysreedharg@users.noreply.github.com>
2021-01-22 16:33:52 +0530

    Update and rename DFS Graph Traversal.py to BFS & DFS Graph Traversal.py

--- BFS & DFS Graph Traversal.py
+++ BFS & DFS Graph Traversal.py
@@ -9,17 +9,25 @@ class Node:
         for child in self.children:
             child.dfsgraph(array)
         return array
+    def bfsgraph(self,array):
+        queue=[self]
+        
+        while len(queue)>0:
+            current=queue.pop(0)
+            array.append(current.name)
+            for child in current.children:
+                queue.append(child)
+        return array
+
 
 new= Node("A")
 new.addchild("B")
 new.addchild("C")
 new.addchild("D")
-
 new.children[0].addchild("E")
-new.children[0].children[0].addchild("I")
-new.children[0].children[0].addchild("J")
-
-
-
-
+new.children[0].addchild("F")
+new.children[0].children[1].addchild("I")
+new.children[0].children[1].addchild("J")
 print(new.dfsgraph([]))
+print(new.bfsgraph([]))
+
