f5d0c4c53290b1f6eb6024f5a509b76ee5f52a79
SUJAY S <44267241+sujaysreedharg@users.noreply.github.com>
2021-01-23 10:05:31 +0530

    Update Network delay Time ( Dijkstra's Algorithm ( Single source shortest path)).py

--- Network delay Time ( Dijkstra's Algorithm ( Single source shortest path)).py
+++ Network delay Time ( Dijkstra's Algorithm ( Single source shortest path)).py
@@ -26,3 +26,29 @@ class Solution:
                 dist[neigh]=min(dist[neigh],dist[cand_node]+dis)
         ans=max(dist.values())
         return ans if ans<float("inf") else -1
+
+    
+    
+    Time-> O(Elog(E)) Space -> O(N+E)   -> Adjacency list and priority queue  (Min heap) Solution:
+    
+     
+    class Solution:
+    def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
+        graph=collections.defaultdict(list)
+        for edges in times:
+            if edges[0] not in graph:
+                graph[edges[0]]=[(edges[1],edges[2])]
+            else:
+                graph[edges[0]].append((edges[1],edges[2]))
+
+        visitedanddist={}
+        pq=[(0,K)]
+        while pq:
+            dis,node= heapq.heappop(pq)
+            if node in visitedanddist:
+                continue
+            visitedanddist[node]=dis
+            for neigh,d in graph[node]:
+                if neigh not in visitedanddist:
+                    heapq.heappush(pq,(dis+d,neigh))
+        return max(visitedanddist.values()) if len(visitedanddist)==N else -1
