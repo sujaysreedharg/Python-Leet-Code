92cf8b4d5cc414ca5256d786a30994ac5661f102
SUJAY S <44267241+sujaysreedharg@users.noreply.github.com>
2021-01-25 15:02:14 +0530

    Update Knapsack Problem.py

--- Knapsack Problem.py
+++ Knapsack Problem.py
@@ -1,3 +1,36 @@
+Memoization:
+    
+class solu:
+    def knapsack(self,profits,weights,capacity):
+        dp = [[-1 for i in range(capacity+1)] for  j in range(len(profits))]
+        
+        return self.knapsackrec(dp,profits,weights,capacity,0),dp
+    def knapsackrec(self,dp,profits,weights,capacity,curridx):
+        if capacity<=0 or curridx==len(weights):
+            return 0
+        if dp[curridx][capacity]!=-1:
+            return dp[curridx][capacity]
+        profit1=0
+        if weights[curridx]<=capacity:
+            profit1=profits[curridx]+self.knapsackrec(dp,profits,weights,capacity-weights[curridx],curridx+1)
+        profit2=self.knapsackrec(dp,profits,weights,capacity,curridx+1)
+        dp[curridx][capacity]= max(profit1,profit2)
+       
+        return dp[curridx][capacity]
+
+        
+
+
+sol=solu()
+print(sol.knapsack([1, 6, 10, 16], [1, 2, 3, 5], 7))
+
+
+Time -> O(N*C) only the DP Array Time
+Space-> O((N*C)+N))  O(N) for recursion call stack -> O((N*C))
+
+
+Bottom-up:
+
 class Solution:
     def knapsack(self,items,capacity):
         knapsackvalues=[[0 for i in range(capacity+1)]for j in range(len(items)+1)]
