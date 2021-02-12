fe90b050365838efdb620c82f091af67a4514e06
SUJAY S <44267241+sujaysreedharg@users.noreply.github.com>
2021-02-12 19:36:14 +0530

    Update Fibonacci (Dynamic Prog).py

--- Fibonacci (Dynamic Prog).py
+++ Fibonacci (Dynamic Prog).py
@@ -1,16 +1,41 @@
-def bootmupfib(n):
-  if n==1 or n==2:
-    return 1
-  bottomup= [None]*(n+1)
-  bottomup[1]=1
-  bottomup[2]=1
-  for i in range(3,n+1):
-    bottomup[i]= bottomup[i-1]+bottomup[i-2]
-  return bottomup
+Botto-up:
 
-
-print(bootmupfib(20))
+def fib(n):
+    dp = [0 for i in range(n+1)]
+    dp[0]= 0
+    dp[1]=1
+    if n==0:
+        return dp[0]
+    if n==1:
+        return dp[1]
+    for i in range(2,n+1):
+        dp[i] = dp[i-1]+dp[i-2]
+    return dp[-1]
+print(fib(10))
 
 
 
 O(n) -> Time
+
+
+
+Top-down:
+
+  
+  
+n=int(input())
+
+def fib(n,dp):
+    
+    if dp[n] is not None:
+        return dp[n]
+    if n==1 or n==2:
+        return 1
+    result= fib(n-1,dp)+fib(n-2,dp)
+    dp[n]=result
+    print(dp)
+    return result
+def findfib(n):
+    dp = [None for i in range(n+1)]
+    return fib(n,dp)
+print(findfib(10))
